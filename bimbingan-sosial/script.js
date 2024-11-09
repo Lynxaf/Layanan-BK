a37690d2b10a9294968a6ee695b7fa459abe3d1d
Mukhlishah Afdhaliyah <116530302+Lynxaf@users.noreply.github.com>
2024-11-09 19:04:47 +0800

    Update script.js

--- bimbingan-sosial/script.js
+++ bimbingan-sosial/script.js
@@ -1,8 +1,3 @@
-// Misalnya, jika ingin menambah interaksi lebih lanjut di website
-document.querySelector('button').addEventListener('click', function() {
-    alert('Terima kasih telah menghubungi kami!');
-  });
-  
   document.addEventListener("DOMContentLoaded", function () {
     const contactForm = document.querySelector("#contact form");
     contactForm.addEventListener("submit", function (event) {
