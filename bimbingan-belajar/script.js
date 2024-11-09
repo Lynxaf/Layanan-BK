d7c0efbaeb15688194af88d22bbbe7bcfd63c4e3
Mukhlishah Afdhaliyah <116530302+Lynxaf@users.noreply.github.com>
2024-11-09 19:07:08 +0800

    Update script.js

--- bimbingan-belajar/script.js
+++ bimbingan-belajar/script.js
@@ -1,9 +1,4 @@
-// Misalnya, jika ingin menambah interaksi lebih lanjut di website
-document.querySelector('button').addEventListener('click', function() {
-    alert('Terima kasih telah menghubungi kami!');
-  });
-  
-  document.addEventListener("DOMContentLoaded", function () {
+   document.addEventListener("DOMContentLoaded", function () {
     const contactForm = document.querySelector("#contact form");
     contactForm.addEventListener("submit", function (event) {
         event.preventDefault();
